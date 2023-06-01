0bf02f977670f84e81116fffb909eadd40b3f9ea
Taha <iamtahamurtaza@gmail.com>
2023-06-01 15:52:11 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './App/store';
+
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.js"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+          <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
